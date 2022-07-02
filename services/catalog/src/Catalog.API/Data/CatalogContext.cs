6c61df22e0ca500ef9b06586ecd9a7d5ea7c73ff
David Alvarado <dealvaradoh@gbm.com.mx>
2022-07-02 17:40:53 -0500

    Adds products catalog data layer

---
+++ services/catalog/src/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,28 @@
+﻿using Ardalis.GuardClauses;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private const string ConnectionStringEnv = "DatabaseSettings:ConnectionString";
+        private const string DatabaseNameEnv = "DatabaseSettings:DatabaseName";
+        private const string CollectionNameEnv = "DatabaseSettings:CollectionName";
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            Guard.Against.Null(configuration);
+
+            var connectionString = configuration.GetValue<string>(ConnectionStringEnv);
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase(DatabaseNameEnv);
+
+            Products = database.GetCollection<Product>(CollectionNameEnv);
+
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
