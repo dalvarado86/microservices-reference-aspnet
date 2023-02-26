edad1604fa7dd72e1157f1796f19ae58a024f34e
David Alvarado <dealvaradoh@gbm.com.mx>
2023-02-26 12:33:49 -0600

    Adds orders domain

---
+++ services/orders/src/Orders.Domain/SeedWork/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace Orders.Domain.SeedWork
+{
+    public abstract class Entity
+    {
+        public int Id { get; protected set; }
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
