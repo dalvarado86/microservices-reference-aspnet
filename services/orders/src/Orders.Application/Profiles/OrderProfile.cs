367b784e17bc6ca09ddd751ff7a0c35b1d1ec09f
David Alvarado <dealvaradoh@gbm.com.mx>
2023-02-26 12:34:13 -0600

    Adds automaper order profile

---
+++ services/orders/src/Orders.Application/Profiles/OrderProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Orders.Domain.Entities;
+
+namespace Orders.Application.Profiles
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            //CreateMap<Order, OrdersVm>().ReverseMap();
+            //CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            //CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
+    }
+}
