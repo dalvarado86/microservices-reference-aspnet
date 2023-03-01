61a30c42227f135a482bac1bd5e7a8fb1f1e8fce
dalvarado86 <dealvarado86@gmail.com>
2023-02-28 21:01:08 -0500

    Adds application services registration

---
+++ services/orders/src/Orders.Application/ApplicationServices.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Orders.Application.Behaviours;
+using System.Reflection;
+
+namespace Orders.Application
+{
+    public static class ApplicationServices
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+            });
+
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            return services;
+        }
+    }
+}
